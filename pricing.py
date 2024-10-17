b64efc87edb13a8885b6a188598e039cecd87071
Felipe Castillo Aravena <fcastilloaravena@gmail.com>
2024-10-17 22:21:54 +0200

    cambie el html

--- pricing.py
+++ pricing.py
@@ -11,8 +11,8 @@ st.title("Expected Ride Duration vs Historical Cost of Ride")
 # Cargar datos
 data = pd.read_csv("Data/dynamic_pricing.csv")
 
-# Sección 1: Diseño en una sola columna unida
-st.image("img/demanda.png", caption="Descripción de la imagen", use_column_width=True,width=500)
+# Sección 1: Diseño en una sola columna pero con imagen controlada en ancho
+st.image("img/demanda.png", caption="Descripción de la imagen", width=500)  # Ajustar el ancho de la imagen
 
 # Sección 2: Mostrar los datos y el gráfico de dispersión en dos columnas
 col1, col2 = st.columns(2)
